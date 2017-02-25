f169b16edf159e80af4faf9d7e94565124090e0c
Brook Hutchinson <brookhutchinson@outlook.com>
2017-02-25 07:43:04 -0600

    Cleanup Project Files, Change package.json "version" property from "0.0.0" to "1.0.0", Remove whitespace, Format files, Create new folders under "src/app", Move "styles.css" from folder "src" to folder "src/app/css", Change "styles" property value file path to "app/css/styles.css" inside angular-cli.json

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,8 @@
+// modules
 import { TestBed, async } from '@angular/core/testing';
 
-import { AppComponent } from './app.component';
+// components
+import { AppComponent }   from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,15 +1,14 @@
 /**
- * This file includes polyfills needed by Angular and is loaded before the app.
- * You can add your own extra polyfills to this file.
+ * This file includes polyfills needed by Angular and is loaded before the app
+ * You can add your own extra polyfills to this file
  *
  * This file is divided into 2 sections:
- *   1. Browser polyfills. These are applied before loading ZoneJS and are sorted by browsers.
- *   2. Application imports. Files imported after ZoneJS that should be loaded before your main
- *      file.
+ *   1. Browser polyfills. These are applied before loading ZoneJS and are sorted by browsers
+ *   2. Application imports. Files imported after ZoneJS that should be loaded before your main file
  *
  * The current setup is for so-called "evergreen" browsers; the last versions of browsers that
  * automatically update themselves. This includes Safari >= 10, Chrome >= 55 (including Opera),
- * Edge >= 13 on the desktop, and iOS 10 and Chrome on mobile.
+ * Edge >= 13 on the desktop, and iOS 10 and Chrome on mobile
  *
  * Learn more in https://angular.io/docs/ts/latest/guide/browser-support.html
  */
@@ -18,7 +17,7 @@
  * BROWSER POLYFILLS
  */
 
-/** IE9, IE10 and IE11 requires all of the following polyfills. **/
+/** IE9, IE10 and IE11 requires all of the following polyfills **/
 // import 'core-js/es6/symbol';
 // import 'core-js/es6/object';
 // import 'core-js/es6/function';
@@ -34,26 +33,26 @@
 // import 'core-js/es6/set';
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
-// import 'classlist.js';  // Run `npm install --save classlist.js`.
+// import 'classlist.js';  // Run `npm install --save classlist.js`
 
-/** IE10 and IE11 requires the following to support `@angular/animation`. */
-// import 'web-animations-js';  // Run `npm install --save web-animations-js`.
+/** IE10 and IE11 requires the following to support `@angular/animation` */
+// import 'web-animations-js';  // Run `npm install --save web-animations-js`
 
 
-/** Evergreen browsers require these. **/
+/** Evergreen browsers require these **/
 import 'core-js/es6/reflect';
 import 'core-js/es7/reflect';
 
 
-/** ALL Firefox browsers require the following to support `@angular/animation`. **/
-// import 'web-animations-js';  // Run `npm install --save web-animations-js`.
+/** ALL Firefox browsers require the following to support `@angular/animation` **/
+// import 'web-animations-js';  // Run `npm install --save web-animations-js`
 
 
 
 /***************************************************************************************************
- * Zone JS is required by Angular itself.
+ * Zone JS is required by Angular itself
  */
-import 'zone.js/dist/zone';  // Included with Angular CLI.
+import 'zone.js/dist/zone';  // Included with Angular CLI
 
 
 
@@ -62,7 +61,7 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
  */
 
 /**
- * Date, currency, decimal and percent pipes.
+ * Date, currency, decimal and percent pipes
  * Needed for: All but Chrome, Firefox, Edge, IE11 and Safari 10
  */
-// import 'intl';  // Run `npm install --save intl`.
+// import 'intl';  // Run `npm install --save intl`
