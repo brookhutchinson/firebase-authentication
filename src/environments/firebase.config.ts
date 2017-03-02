12e0bcb32e2a5cfe49e8eabd8ec7e13064f0ef35
Brook Hutchinson <brookhutchinson@outlook.com>
2017-03-02 09:17:46 -0600

    Create Firebase Configuration File named "firebase.config.ts" inside folder "src/environments", Define const named "firebaseConfig" inside "firebase.config.ts", Define firebase database configuration properties inside "firebase.config.ts"

---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,7 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAiqkL5P7NAnXnV_pBittUWqvHVrin9Omk",
+  authDomain: "fir-authentication-5dfdb.firebaseapp.com",
+  databaseURL: "https://fir-authentication-5dfdb.firebaseio.com",
+  storageBucket: "fir-authentication-5dfdb.appspot.com",
+  messagingSenderId: "35818511720"
+}
