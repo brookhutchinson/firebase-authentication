6e9d07db6c0703ae1ae1746e3b783da0e897ede9
Brook Hutchinson <brookhutchinson@outlook.com>
2017-03-02 09:29:29 -0600

    Setup connection between Angular App and the Angular Firebase Project, Import Service named "AngularFireModule" into app.module.ts, Import Firebase Configuration Object from firebase.config.ts into app.module.ts, Add AngularFireModule to the @NgModule Decorator "imports" array inside app.module.ts, Configure the AngularFireModule using initializeApp() factory method and the "firebaseConfig" object

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
 // modules
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
-import { HttpModule }    from '@angular/http';
+import { NgModule }          from '@angular/core';
+import { BrowserModule }     from '@angular/platform-browser';
+import { FormsModule }       from '@angular/forms';
+import { HttpModule }        from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
 // components
-import { AppComponent }  from './app.component';
+import { AppComponent }      from './app.component';
+
+// firebase config
+import { firebaseConfig }    from './../environments/firebase.config';
 
 @NgModule({
   // modules
-  imports: [ BrowserModule, FormsModule, HttpModule ],
+  imports: [ BrowserModule, FormsModule, HttpModule, AngularFireModule.initializeApp(firebaseConfig) ],
   // components
   declarations: [ AppComponent ],
   // services
