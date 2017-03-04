08db789de0ffe7f2c836d94e0af28abbed42b2c6
Brook Hutchinson <brookhutchinson@outlook.com>
2017-03-04 09:35:26 -0600

    Use AuthService and Firebase Authentication Service to check status of user authentication, Based on whether user is authenticated route the user to either HomeComponent or LoginComponent, Import AuthService and Router Module into app.component.ts, Inject AuthService and Router Module into constructor() function, Define property named isLoggedIn as Boolean inside AppComponent Class, Use AuthService to check if user is currently authenticated, Check for any changes to the authentication status of the user, Set the value of this.isLoggedIn property , If user is logged in then use router to navigate user to Home Route '', If user is not logged in then use router to navigate user to Login Route 'login', Define userDisplayName property value, Define userDisplayEmail property value, When user is logged in use the auth object to set userDisplayName property value, When user is logged in use the auth object to set userDisplayEmail property value, When user is not logged in set userDisplayName property value to '', When user is not logged in set userDisplayEmail property value to ''

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,50 @@
-import { Component } from '@angular/core';
+// modules
+import { Router }      from '@angular/router';
+
+// components
+import { Component }   from '@angular/core';
+
+// services
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+  // define properties
+  private isLoggedIn: Boolean;
+  private userDisplayName: String;
+  private userDisplayEmail: String;
+
+  constructor(public authService: AuthService, private router: Router) {
+     // check authentication status of user
+     this.authService.af.auth.subscribe(
+       (auth) => {
+         // check authentication
+         if (auth == null) {
+           // user is not logged in, not authenticated
+
+           // set properties
+           this.isLoggedIn = false;
+           this.userDisplayName = '';
+           this.userDisplayEmail = '';
+
+           // use router to navigate user to login route
+           this.router.navigate(['login']);
+         } else {
+           // user is logged in, authenticated
+
+           // set properties
+           this.isLoggedIn = true;
+           this.userDisplayName = auth.google.displayName;
+           this.userDisplayEmail = auth.google.email;
+
+           // use router to navigate user to home route
+           this.router.navigate(['']);
+         }
+       }
+     );
+  }
+}
